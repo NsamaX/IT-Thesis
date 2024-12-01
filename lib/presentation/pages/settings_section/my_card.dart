0992f02b3b2a522dbe64c196eda9072a958943ac
NsamaX <vijuksama@gmail.com>
2024-12-01 17:13:40 +0700

    .

--- lib/presentation/pages/settings_section/my_card.dart
+++ lib/presentation/pages/settings_section/my_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../core/locales/localizations.dart';
+import '../../blocs/NFC.dart';
 import '../../widgets/bar/app.dart';
+import '../../widgets/card/card.dart';
 
 class MyCardPage extends StatelessWidget {
   @override
@@ -11,6 +14,31 @@ class MyCardPage extends StatelessWidget {
           Icons.arrow_back_ios_new_rounded: '/back',
           AppLocalizations.of(context).translate('my_card.title'): null,
           null: null,
+        },
+      ),
+      body: BlocBuilder<NFCCubit, NFCState>(
+        builder: (context, state) {
+          if (state.savedTags == null || state.savedTags!.isEmpty) {
+            return Container();
+          }
+
+          final savedCards =
+              state.savedTags!.map((map) => map.values.first).toList();
+
+          return GridView.builder(
+            padding: const EdgeInsets.all(16),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 3 / 4,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            itemCount: savedCards.length,
+            itemBuilder: (context, index) {
+              final card = savedCards[index];
+              return CardWidget(card: card);
+            },
+          );
         },
       ),
     );
--- lib/presentation/widgets/card/card.dart
+++ lib/presentation/widgets/card/card.dart
@@ -6,19 +6,21 @@ import '../../blocs/deck_manager.dart';
 
 class CardWidget extends StatelessWidget {
   final CardEntity card;
-  final int count;
+  final int? count;
 
   const CardWidget({
     Key? key,
     required this.card,
-    required this.count,
+    this.count = 0,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final isSelected = context.watch<DeckManagerCubit>().state.selectedCard == card;
-    final isNfcReadEnabled = context.watch<DeckManagerCubit>().state.isNfcReadEnabled;
+    final isSelected =
+        context.watch<DeckManagerCubit>().state.selectedCard == card;
+    final isNfcReadEnabled =
+        context.watch<DeckManagerCubit>().state.isNfcReadEnabled;
     return Stack(
       children: [
         GestureDetector(
@@ -70,13 +72,14 @@ class CardWidget extends StatelessWidget {
             ),
           ),
         ),
-        if (context.watch<DeckManagerCubit>().state.isEditMode && !isNfcReadEnabled)
+        if (context.watch<DeckManagerCubit>().state.isEditMode &&
+            !isNfcReadEnabled)
           Positioned(
             top: 0,
             right: 0,
             child: Column(
               children: [
-                buildCount(context, count),
+                buildCount(context, count!),
                 buildButton(
                   context,
                   Icons.add,
