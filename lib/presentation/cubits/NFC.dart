b8ab5033c42537bc4e668bffcf24129529323d7b
NsamaX <vijuksama@gmail.com>
2024-12-28 22:18:01 +0700

    .

--- lib/presentation/cubits/NFC.dart
+++ lib/presentation/cubits/NFC.dart
@@ -8,60 +8,58 @@ import 'package:nfc_project/domain/usecases/tag.dart';
 class NFCState {
   final bool isNFCEnabled;
   final bool isProcessing;
-  final TagEntity? lastReadTag;
-  final String? errorMessage;
   final bool isOperationSuccessful;
+  final String? errorMessage;
+  final TagEntity? lastReadTag;
   final List<Map<TagEntity, CardEntity>>? savedTags;
 
   NFCState({
     required this.isNFCEnabled,
     this.isProcessing = false,
-    this.lastReadTag,
-    this.errorMessage,
     this.isOperationSuccessful = false,
+    this.errorMessage,
+    this.lastReadTag,
     this.savedTags,
   });
 
   NFCState copyWith({
     bool? isNFCEnabled,
     bool? isProcessing,
-    TagEntity? lastReadTag,
-    String? errorMessage,
     bool? isOperationSuccessful,
+    String? errorMessage,
+    TagEntity? lastReadTag,
     List<Map<TagEntity, CardEntity>>? savedTags,
   }) {
     return NFCState(
       isNFCEnabled: isNFCEnabled ?? this.isNFCEnabled,
       isProcessing: isProcessing ?? this.isProcessing,
-      lastReadTag: lastReadTag ?? this.lastReadTag,
+      isOperationSuccessful: isOperationSuccessful ?? this.isOperationSuccessful,
       errorMessage: errorMessage ?? this.errorMessage,
-      isOperationSuccessful:
-          isOperationSuccessful ?? this.isOperationSuccessful,
+      lastReadTag: lastReadTag ?? this.lastReadTag,
       savedTags: savedTags ?? this.savedTags,
     );
   }
 }
 
 class NFCCubit extends Cubit<NFCState> {
-  final SaveTagUseCase saveTagUseCase;
-  final LoadTagsUseCase loadTagsUseCase;
   final Logger logger = Logger();
 
-  static const String nfcUnavailableMessage =
-      'NFC is not available on this device.';
-  static const String tagNotWritableMessage =
-      'The detected tag is not writable. Please try another tag.';
-  static const String tagTooLargeMessage =
-      'The data exceeds the tag\'s capacity. Please reduce the size of the data.';
+  final SaveTagUseCase saveTagUseCase;
+  final LoadTagsUseCase loadTagsUseCase;
+
+  static const String nfcUnavailableMessage = 'NFC is not available on this device.';
+  static const String tagNotWritableMessage = 'The detected tag is not writable. Please try another tag.';
+  static const String tagTooLargeMessage = 'The data exceeds the tag\'s capacity. Please reduce the size of the data.';
 
   NFCCubit({
     required this.saveTagUseCase,
     required this.loadTagsUseCase,
   }) : super(NFCState(isNFCEnabled: false, savedTags: []));
 
+  //----------------------------- State Control ------------------------------//
+
   @override
   Future<void> close() async {
-    logger.i('NFCCubit is being closed.');
     return super.close();
   }
 
@@ -71,7 +69,6 @@ class NFCCubit extends Cubit<NFCState> {
 
   void toggleNFC() {
     emitSafe(state.copyWith(isNFCEnabled: !state.isNFCEnabled));
-    logger.i('NFC toggled: ${state.isNFCEnabled ? "Enabled" : "Disabled"}');
   }
 
   void resetOperationStatus() {
@@ -81,6 +78,8 @@ class NFCCubit extends Cubit<NFCState> {
   void clearErrorMessage() {
     emitSafe(state.copyWith(errorMessage: null));
   }
+
+  //---------------------------- Session Control -----------------------------//
 
   Future<void> start({CardEntity? card}) async {
     logger.i(card == null
@@ -93,8 +92,7 @@ class NFCCubit extends Cubit<NFCState> {
     }
   }
 
-  Future<void> _startSession(
-      Future<void> Function(NfcTag tag) onDiscovered) async {
+  Future<void> _startSession(Future<void> Function(NfcTag tag) onDiscovered) async {
     if (state.isProcessing) {
       logger.w('Another NFC process is ongoing.');
       return;
@@ -134,28 +132,24 @@ class NFCCubit extends Cubit<NFCState> {
     }
   }
 
+  //------------------------------ Read Session ------------------------------//
+
   Future<void> _processRead(NfcTag tag) async {
     logger.i('Tag discovered: ${tag.data}');
     final ndef = Ndef.from(tag);
     if (ndef == null) {
-      throw Exception(
-          'This NFC tag is not supported. Please use a compatible tag.');
+      throw Exception('This NFC tag is not supported. Please use a compatible tag.');
     }
-
-    // ตรวจสอบโครงสร้างของ tag.data ก่อนประมวลผล
     final ndefMessage = tag.data['ndef']?['cachedMessage'];
     if (ndefMessage == null) {
       throw Exception('No NDEF message found on the tag.');
     }
-
     final records = ndefMessage['records'] as List<dynamic>;
     final parsedRecords = records.map((record) {
       final payload = record['payload'] as List<int>;
-      return String.fromCharCodes(payload).substring(3); // ข้าม `en` (ภาษา)
+      return String.fromCharCodes(payload).substring(3);
     }).toList();
-
     logger.i('Parsed Records: $parsedRecords');
-
     final cardIdMatch = parsedRecords
         .firstWhere((record) => record.startsWith('Card ID:'), orElse: () => '')
         .split(': ')
@@ -164,58 +158,37 @@ class NFCCubit extends Cubit<NFCState> {
         .firstWhere((record) => record.startsWith('Game:'), orElse: () => '')
         .split(': ')
         .last;
-
     final tagId = _extractTagId(tag);
-
     final tagEntity = TagEntity(
       tagId: tagId,
       cardId: cardIdMatch,
       game: gameMatch,
       timestamp: DateTime.now(),
     );
-
     logger.i('Tag Read Successful: $tagEntity');
-    emitSafe(
-      state.copyWith(lastReadTag: tagEntity, isOperationSuccessful: true),
+    emitSafe(state.copyWith(lastReadTag: tagEntity, isOperationSuccessful: true),
     );
-    }
+  }
+
+  //------------------------------ Write Session -----------------------------//
 
   Future<void> _processWrite(NfcTag tag, CardEntity card) async {
     logger.i('Tag discovered for writing: ${tag.data}');
     final ndef = Ndef.from(tag);
     if (ndef == null) throw Exception('This tag does not support NDEF.');
     if (!ndef.isWritable) throw Exception('This tag is read-only.');
-
-    // สร้างข้อความ NDEF
     final ndefMessage = NdefMessage([
       NdefRecord.createText('Card Name: ${card.name}'),
       NdefRecord.createText('Game: ${card.game}'),
       NdefRecord.createText('Card ID: ${card.cardId}'),
     ]);
-
     if (ndefMessage.byteLength > ndef.maxSize)
       throw Exception('Data exceeds tag capacity.');
-
-    // แสดงข้อความ NDEF ก่อนเขียน
     for (var record in ndefMessage.records) {
       logger.i('Writing record: ${String.fromCharCodes(record.payload)}');
     }
-
-    // เขียนข้อความลงแท็ก
     await ndef.write(ndefMessage);
     logger.i('NFC Write Successful.');
-
-    // สร้าง TagEntity
-    final tagId = _extractTagId(tag);
-    final tagEntity = TagEntity(
-      tagId: tagId,
-      cardId: card.cardId,
-      game: card.game,
-      timestamp: DateTime.now(),
-    );
-
-    await _saveTagWithCard(tagEntity, card);
-
     emitSafe(state.copyWith(isProcessing: false, isOperationSuccessful: true));
     await stopSession(reason: 'Tag written successfully');
   }
@@ -228,22 +201,9 @@ class NFCCubit extends Cubit<NFCState> {
     return tagId;
   }
 
-  Map<String, String?> _parseGameAndCardId(dynamic data) {
-    // แปลงข้อมูล data เป็น string หากเป็น binary
-    final payloadString = String.fromCharCodes(data);
+  //------------------------------- Tag Session ------------------------------//
 
-    // สร้าง regex เพื่อแยกข้อมูล
-    final cardIdMatch = RegExp(r'Card ID: (\d+)').firstMatch(payloadString);
-    final gameMatch = RegExp(r'Game: (.+)').firstMatch(payloadString);
-
-    return {
-      'cardId': cardIdMatch?.group(1),
-      'game': gameMatch?.group(1),
-    };
-  }
-
-  Future<void> _saveTagWithCard(
-      TagEntity tagEntity, CardEntity cardEntity) async {
+  Future<void> scanHistory(TagEntity tagEntity, CardEntity cardEntity) async {
     try {
       await saveTagUseCase(tagEntity, cardEntity);
       logger.i('Tag and Card saved successfully.');
@@ -259,8 +219,7 @@ class NFCCubit extends Cubit<NFCState> {
       final tagsWithCards = await loadTagsUseCase();
       emitSafe(state.copyWith(savedTags: tagsWithCards));
     } catch (e) {
-      emitSafe(
-          state.copyWith(errorMessage: 'Error loading tags and cards: $e'));
+      emitSafe(state.copyWith(errorMessage: 'Error loading tags and cards: $e'));
     }
   }
 }
